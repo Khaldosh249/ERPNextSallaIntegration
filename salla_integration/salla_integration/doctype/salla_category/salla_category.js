bf8d13031121b54e45a5318ca2d817275c7784a9
Khaldosh249 <khalid.wad.rashad@gmail.com>
2026-01-21 12:33:27 +0300

    feat: delete sync one category from salla

--- salla_integration/salla_integration/doctype/salla_category/salla_category.js
+++ salla_integration/salla_integration/doctype/salla_category/salla_category.js
@@ -1,19 +1,9 @@
 // Copyright (c) 2026, Khaldosh249 and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Salla Category", {
-	refresh(frm) {
-        frm.add_custom_button("Sync Categories from Salla", () => {
-            frappe.call({
-                method: "salla_integration.api.sync_categories.sync_a_category_to_salla",
-                args: {
-                    category: frm.doc
-                },
-                callback: function(response) {
-                    frappe.msgprint("Categories synced successfully!");
-                }
-            });
-        });
-	},
-});
+// frappe.ui.form.on("Salla Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
 
